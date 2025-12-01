2d0b3af98d43b668d4863b0385e31933798e8375
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-12-01 10:32:14 -0600

    feat: Day One Part 1

--- day_one/day_one.go
+++ day_one/day_one.go
@@ -1,1 +1,44 @@
 package dayone
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func PartOne(nums []int) int {
+	result := 0
+	displacement := 0
+	for _, n := range nums {
+		displacement += n
+		if displacement%100 == 50 || displacement%100 == -50 {
+			result++
+		}
+	}
+	return result
+}
+
+func ParseInput(path string) ([]int, error) {
+	input, err := os.ReadFile(path)
+	if err != nil {
+		return []int{}, err
+	}
+
+	lines := strings.Split(string(input), "\n")
+	result := make([]int, len(lines)-1)
+	for i, line := range lines[:len(lines)-1] {
+		if len(line) == 0 {
+			continue
+		}
+		direction, distance := line[:1], line[1:]
+		n, err := strconv.Atoi(distance)
+		if err != nil {
+			return []int{}, err
+		}
+		if direction == "L" {
+			n = n * -1
+		}
+		result[i] = n
+	}
+	return result, nil
+}
--- day_one/day_one_test.go
+++ day_one/day_one_test.go
@@ -1,1 +1,35 @@
 package dayone_test
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	dayone "github.com/cameronharro/advent-of-code-2025/day_one"
+)
+
+func TestParseInput(t *testing.T) {
+	numbers, err := dayone.ParseInput("./day_one_test.txt")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	expected := []int{-68, -30, 48, -5, 60, -55, -1, -99, 14, -82}
+	if !slices.Equal(numbers, expected) {
+		t.Errorf("ParseInput() got %v expected %v", numbers, expected)
+	}
+}
+
+func TestPartOne(t *testing.T) {
+	testData := []int{-68, -30, 48, -5, 60, -55, -1, -99, 14, -82}
+	result := dayone.PartOne(testData)
+	if result != 3 {
+		t.Errorf("PartOne() got %d, expected 3", result)
+	}
+
+	data, err := dayone.ParseInput("./day_one.txt")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	result = dayone.PartOne(data)
+	fmt.Println(result)
+}
