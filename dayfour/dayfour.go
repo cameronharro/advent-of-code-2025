eeb0d1191bb1175713cd395c0c77244f512a50a0
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-12-04 09:41:51 -0600

    feat: GetSurrounding()

--- dayfour/dayfour.go
+++ dayfour/dayfour.go
@@ -18,3 +18,32 @@ func ParseInput(path string) ([][]string, error) {
 	}
 	return result, nil
 }
+
+type Point struct {
+	X int
+	Y int
+}
+
+func GetSurrounding(point Point, grid [][]string) []string {
+	positions := []Point{}
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			if x == 0 && y == 0 {
+				continue
+			}
+			positions = append(positions, Point{X: point.X + x, Y: point.Y + y})
+		}
+	}
+
+	result := make([]string, 8)
+	validPoints := 0
+	for _, position := range positions {
+		if position.X < 0 || position.Y < 0 || position.X > len(grid[0])-1 || position.Y > len(grid)-1 {
+			continue
+		}
+		result[validPoints] = grid[position.Y][position.X]
+		validPoints++
+	}
+
+	return result[:validPoints]
+}
--- dayfour/dayfour_test.go
+++ dayfour/dayfour_test.go
@@ -1,6 +1,8 @@
 package dayfour_test
 
 import (
+	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/cameronharro/advent-of-code-2025/dayfour"
@@ -15,5 +17,43 @@ func TestParseInput(t *testing.T) {
 	expectedLength := 10
 	if len(result) != expectedLength || len(result[0]) != expectedLength {
 		t.Errorf("ParseInput() got %dx%d grid, expected %dx%d\n", len(result), len(result[0]), expectedLength, expectedLength)
+	}
+}
+
+func TestGetSurrounding(t *testing.T) {
+	grid, err := dayfour.ParseInput("./dayfour_test.txt")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	type TestCase struct {
+		inputPoint dayfour.Point
+		expected   []string
+	}
+
+	testCases := []TestCase{
+		{
+			inputPoint: dayfour.Point{
+				X: 2,
+				Y: 0,
+			},
+			expected: []string{".", "@", "@", "@", "."},
+		},
+		{
+			inputPoint: dayfour.Point{
+				X: 4,
+				Y: 2,
+			},
+			expected: []string{".", "@", "@", "@", "@", ".", ".", "@"},
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			result := dayfour.GetSurrounding(testCase.inputPoint, grid)
+			if !slices.Equal(result, testCase.expected) {
+				t.Errorf("GetSurrounding() got %v, expected %v", result, testCase.expected)
+			}
+		})
 	}
 }
